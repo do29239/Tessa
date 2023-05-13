5306ab86427555c36800c431ea2d2beef5de5bff
do29239 <do29239@seeu.edu.mk>
2023-05-13 15:44:55 +0200

    [Added neccesary middlewares for all user roles and added route groups in web.php]

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,22 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::middleware('admin')->group(function () {
+
+
+
+    });
+    Route::middleware('stylist')->group(function () {
+
+
+
+    });
+    Route::middleware('user')->group(function () {
+
+
+
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
