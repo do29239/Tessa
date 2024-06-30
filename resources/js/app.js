fa2e6dbf0a0617cb5f13e1e1901b973897bf2fab
nazifselimi <nazif.selimi73@gmail.com>
2024-06-30 17:57:19 +0200

    hopefully made it an app

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,12 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function (registration) {
+        console.log(`SW registered successfully!`);
+    }).catch(function (registrationError) {
+        console.log(`SW registration failed`);
+    });
+}
